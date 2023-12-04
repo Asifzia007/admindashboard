a263d2272d76053b8f10be857f46922fbc9a29a2
Asif <asif.ziakps@gmail.com>
2023-12-04 13:10:59 +0530

    first commit

---
+++ dashboard/src/App.js
@@ -0,0 +1,17 @@
+
+import {  Routes } from 'react-router-dom';
+import './App.css';
+import Sidenav from './components/Sidenav';
+
+function App() {
+  return (
+    <>
+    <Sidenav/>
+    <Routes>
+
+    </Routes>
+    </>
+  );
+}
+
+export default App;
